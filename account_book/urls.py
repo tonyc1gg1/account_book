14f85ffa3820abae90f975a2bc360eaf34cf40e0
tony <tony@gmail.com>
2025-07-12 01:56:45 +0800

    建立超級使用者

--- account_book/urls.py
+++ account_book/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from record import views
 
 urlpatterns = [
+    path("", views.hello),
     path("admin/", admin.site.urls),
 ]
